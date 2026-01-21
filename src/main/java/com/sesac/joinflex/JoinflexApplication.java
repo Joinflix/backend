bc69e683bb878b044fa5d90abdb7738dc2dc9bd0
devYuraKim <dev.yurakim@gmail.com>
2026-01-21 14:06:56 +0900

    first commit

---
+++ src/main/java/com/sesac/joinflex/JoinflexApplication.java
@@ -0,0 +1,13 @@
+package com.sesac.joinflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinflexApplication.class, args);
+	}
+
+}
