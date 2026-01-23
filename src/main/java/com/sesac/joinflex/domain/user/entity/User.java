621b94076c3cbe922d37be872fdd914d7b521dfc
yu-rim98 <rimmm098@gmail.com>
2026-01-23 17:48:09 +0900

    Feat: User entity 필드 추가

--- src/main/java/com/sesac/joinflex/domain/user/entity/User.java
+++ src/main/java/com/sesac/joinflex/domain/user/entity/User.java
@@ -5,17 +5,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
 @Entity
 public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String email;
+
+    private String nickname;
+
+    private String password;
+
+    // role
+
+    // membership
 
 }
