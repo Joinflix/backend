41aac73a2ff244f8d400907f3fc87561622e3dbc
holdrain47 <holdrain47@gmail.com>
2026-01-27 11:43:20 +0900

    Feat: FriendRequest Entity 세팅

--- src/main/java/com/sesac/joinflex/domain/friend/entity/FriendRequest.java
+++ src/main/java/com/sesac/joinflex/domain/friend/entity/FriendRequest.java
@@ -2,15 +2,7 @@ package com.sesac.joinflex.domain.friend.entity;
 
 import com.sesac.joinflex.domain.user.entity.User;
 import com.sesac.joinflex.global.common.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,8 +25,27 @@ public class FriendRequest extends BaseEntity {
     @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;
 
-    @Column(nullable = false)
-    private boolean isAccepted;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private FriendRequestStatus status;
 
+    private FriendRequest(User sender, User receiver) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.status = FriendRequestStatus.PENDING;
+    }
+
+    //생성 & 상태변경 메소드
+
+    public static FriendRequest create(User sender, User receiver) {
+        return new FriendRequest(sender, receiver);
+    }
+
+
+    public void accept(){ this.status = FriendRequestStatus.ACCEPTED; }
+    public void reject(){ this.status = FriendRequestStatus.REJECTED; }
+    public void cancel(){ this.status = FriendRequestStatus.CANCELED; }
+
+    public boolean isPending() {return this.status == FriendRequestStatus.PENDING;}
 
 }
