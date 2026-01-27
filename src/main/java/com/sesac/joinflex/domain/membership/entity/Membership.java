d17937fb83913d64610fb6e64dd070ddc11dc0d6
qqqkyj <123rkdduswn@naver.com>
2026-01-27 10:51:51 +0900

    user 엔티티 수정(생성자만 builder 패턴 적용) membership 엔티티 생성

---
+++ src/main/java/com/sesac/joinflex/domain/membership/entity/Membership.java
@@ -0,0 +1,34 @@
+package com.sesac.joinflex.domain.membership.entity;
+
+import com.sesac.joinflex.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Membership extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true, nullable = false)
+    private MembershipType type;
+
+    private String displayName;
+    private String description;
+    private int price;
+    private String resolution;
+    private int maxConcurrent; // 동시 접속 제한 수치
+
+    @Builder
+    private Membership(int maxConcurrent, String resolution, int price, String description, String displayName, MembershipType type) {
+        this.maxConcurrent = maxConcurrent;
+        this.resolution = resolution;
+        this.price = price;
+        this.description = description;
+        this.displayName = displayName;
+        this.type = type;
+    }
+}
