085b9cb50bd17c7566e6fc5dc40c1bae1d9bd0da
yu-rim98 <rimmm098@gmail.com>
2026-01-29 14:53:01 +0900

    Feat: AuthenticationPrincipal 추가

--- src/main/java/com/sesac/joinflex/domain/notification2/controller/NotificationController.java
+++ src/main/java/com/sesac/joinflex/domain/notification2/controller/NotificationController.java
@@ -1,9 +1,10 @@
 package com.sesac.joinflex.domain.notification2.controller;
 
 import com.sesac.joinflex.domain.notification2.service.NotificationService;
+import com.sesac.joinflex.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -15,9 +16,9 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
-    @GetMapping(value = "/subscribe/{userId}", produces = "text/event-stream")
-    public SseEmitter subscribe(@PathVariable Long userId) {
-        return notificationService.subscribe(userId);
+    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    public SseEmitter subscribe(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return notificationService.subscribe(customUserDetails.getId());
     }
 
 }
