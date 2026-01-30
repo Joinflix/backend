7da0c92e0ef3c38041ede052c7fa1c2fa2ce3aad
yu-rim98 <rimmm098@gmail.com>
2026-01-30 09:41:14 +0900

    Feat: UtilityClass 어노테이션 적용

--- src/main/java/com/sesac/joinflex/domain/notification2/message/InviteMessageTemplate.java
+++ src/main/java/com/sesac/joinflex/domain/notification2/message/InviteMessageTemplate.java
@@ -1,10 +1,12 @@
 package com.sesac.joinflex.domain.notification2.message;
 
 import com.sesac.joinflex.domain.party.entity.PartyRoom;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class InviteMessageTemplate {
 
-    public static String emailBody(PartyRoom room, String joinUrl) {
+    public String emailBody(PartyRoom room, String joinUrl) {
         return String.format("""
                 %s님이 '%s' 파티에 초대했습니다.
                 링크를 클릭해서 입장하세요: %s
@@ -14,7 +16,7 @@ public class InviteMessageTemplate {
             joinUrl);
     }
 
-    public static String notification(String host, String roomName, String guest) {
+    public String notification(String host, String roomName, String guest) {
         return String.format("%s님이 %s님을 '%s' 파티에 초대했습니다.", host, guest, roomName);
     }
 
