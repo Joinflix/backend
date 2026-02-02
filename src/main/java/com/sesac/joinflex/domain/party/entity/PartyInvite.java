4f8cb2eff4c73aab1c93ac30133e07f013d4aa3a
yu-rim98 <rimmm098@gmail.com>
2026-02-02 21:51:13 +0900

    Refactor: 사용되지 않는 필드 제거

--- src/main/java/com/sesac/joinflex/domain/party/entity/PartyInvite.java
+++ src/main/java/com/sesac/joinflex/domain/party/entity/PartyInvite.java
@@ -3,8 +3,6 @@ package com.sesac.joinflex.domain.party.entity;
 import com.sesac.joinflex.domain.user.entity.User;
 import com.sesac.joinflex.global.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -34,17 +32,12 @@ public class PartyInvite extends BaseEntity {
     @JoinColumn(name = "guest_id", nullable = false)
     private User guest;
 
-    @Enumerated(EnumType.STRING)
-    private InviteStatus status;
-
-    private PartyInvite(PartyRoom partyRoom, User guest, InviteStatus status) {
+    private PartyInvite(PartyRoom partyRoom, User guest) {
         this.partyRoom = partyRoom;
         this.guest = guest;
-        this.status = status;
     }
 
     public static PartyInvite create(PartyRoom partyRoom, User guest) {
-        return new PartyInvite(partyRoom, guest, InviteStatus.PENDING);
+        return new PartyInvite(partyRoom, guest);
     }
-
 }
