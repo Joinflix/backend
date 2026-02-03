a7719304c135a08ec268578a080f8fa23ebb4ca7
qqqkyj <123rkdduswn@naver.com>
2026-02-03 17:46:54 +0900

    RefundRepository - REQUESTED(환불 요청), COMPLETED(완료)인 데이터 모두 조회

--- src/main/java/com/sesac/joinflex/domain/refund/repository/RefundRepository.java
+++ src/main/java/com/sesac/joinflex/domain/refund/repository/RefundRepository.java
@@ -5,6 +5,9 @@ import com.sesac.joinflex.domain.refund.entity.Refund;
 import com.sesac.joinflex.domain.refund.entity.RefundStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RefundRepository extends JpaRepository<Refund, Long> {
-    boolean existsByPaymentAndStatus(Payment payment, RefundStatus status);
+    // 쿼리 한 번으로 REQUESTED, COMPLETED 상태를 모두 체크
+    Boolean existsByPaymentAndStatusIn(Payment payment, List<RefundStatus> statuses);
 }
