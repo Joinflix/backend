a0412bb33c14394058ec33ec4e3830d1cf33e95e
yu-rim98 <rimmm098@gmail.com>
2026-02-03 16:34:40 +0900

    Feat: SSE 연결 시 이전 알림 조회 구현

--- src/main/java/com/sesac/joinflex/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/sesac/joinflex/domain/notification/repository/NotificationRepository.java
@@ -1,10 +1,13 @@
 package com.sesac.joinflex.domain.notification.repository;
 
 import com.sesac.joinflex.domain.notification.entity.Notification;
+import com.sesac.joinflex.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
+    List<Notification> findByUser(User user);
 }
