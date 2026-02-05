df2fb4eade25586a6b43309105c8702f56a30caf
yu-rim98 <rimmm098@gmail.com>
2026-02-05 16:33:42 +0900

    Refactor: 반환 시 불필요한 필드 제거

--- src/main/java/com/sesac/joinflex/domain/chat/dto/response/ChatMessageResponse.java
+++ src/main/java/com/sesac/joinflex/domain/chat/dto/response/ChatMessageResponse.java
@@ -5,7 +5,6 @@ import com.sesac.joinflex.domain.chat.dto.MessageType;
 public record ChatMessageResponse(
     MessageType messageType,
     String sender,
-    String message,
-    Integer currentMemberCount
+    String message
 ) {
 }
