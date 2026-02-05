e81a47ef12cfdb2d639b20c36b417d1f038803c2
yu-rim98 <rimmm098@gmail.com>
2026-02-05 11:20:43 +0900

    Feat: 파티방 입장 시 메시지 구현

---
+++ src/main/java/com/sesac/joinflex/domain/chat/dto/response/ChatMessageResponse.java
@@ -0,0 +1,11 @@
+package com.sesac.joinflex.domain.chat.dto.response;
+
+import com.sesac.joinflex.domain.chat.dto.MessageType;
+
+public record ChatMessageResponse(
+    MessageType messageType,
+    String sender,
+    String message,
+    Integer currentMemberCount
+) {
+}
---
+++ src/main/java/com/sesac/joinflex/domain/chat/service/ChatService.java
@@ -0,0 +1,24 @@
+package com.sesac.joinflex.domain.chat.service;
+
+import com.sesac.joinflex.domain.chat.dto.MessageType;
+import com.sesac.joinflex.domain.chat.dto.response.ChatMessageResponse;
+import com.sesac.joinflex.domain.party.entity.PartyRoom;
+import com.sesac.joinflex.domain.party.repository.PartyRoomRepository;
+import com.sesac.joinflex.domain.user.dto.response.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+    private final PartyRoomRepository partyRoomRepository;
+
+    public ChatMessageResponse createEnterMessage(Long partyId, UserResponse user) {
+        PartyRoom partyRoom = partyRoomRepository.findById(partyId).orElseThrow();
+        return new ChatMessageResponse(MessageType.ENTER, user.getNickName(),
+            user.getNickName() + "님이 입장하셨습니다.",
+            partyRoom.getCurrentMemberCount());
+    }
+
+}
