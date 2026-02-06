808194c542038838490a9b4b0022d4ee74131916
holdrain <holdrain47@gmail.com>
2026-02-06 15:37:31 +0900

    refactor: Review 도메인 코드 리팩토링

--- src/main/java/com/sesac/joinflex/domain/review/service/ReviewService.java
+++ src/main/java/com/sesac/joinflex/domain/review/service/ReviewService.java
@@ -54,8 +54,7 @@ public class ReviewService {
 
     @Transactional(readOnly = true)
     public Slice<ReviewResponse> getMovieReviews(Long movieId, Long cursorId, Pageable pageable) {
-        Slice<Review> reviews = reviewRepository.
-                findReviewsByMovieId(
+        Slice<Review> reviews = reviewRepository.findReviewsByMovieId(
                         movieId, cursorId == null ? Long.MAX_VALUE : cursorId, pageable);
 
         return reviews.map(ReviewResponse::from);
@@ -63,8 +62,7 @@ public class ReviewService {
 
     @Transactional(readOnly = true)
     public Slice<ReviewResponse> getUserReviews(Long userId, Long cursorId, Pageable pageable) {
-        Slice<Review> reviews = reviewRepository.
-                findReviewsByUserId(
+        Slice<Review> reviews = reviewRepository.findReviewsByUserId(
                         userId, cursorId == null ? Long.MAX_VALUE : cursorId, pageable);
 
         return reviews.map(ReviewResponse::from);
