eff7a796f7f35c68876671dcf0200c13e40d51cd
holdrain <holdrain47@gmail.com>
2026-02-06 17:05:05 +0900

    refactor: 엔드포인트 정리

--- src/main/java/com/sesac/joinflex/domain/movie/controller/MovieController.java
+++ src/main/java/com/sesac/joinflex/domain/movie/controller/MovieController.java
@@ -13,18 +13,21 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(ApiPath.MOVIE)
 public class MovieController {
 
     private final MovieService movieService;
     private final ReviewService reviewService;
 
     // 전체 영화 조회
-    @GetMapping(ApiPath.MOVIE)
+    // http://localhost:8080/api/movies
+    @GetMapping
     public ResponseEntity<Slice<MovieResponse>> getMovies(
             @PageableDefault(size = 20) Pageable pageable) {
         Slice<MovieResponse> response = movieService.getMovies(pageable);
@@ -32,16 +35,17 @@ public class MovieController {
     }
 
     // 영화 상세 조회
-    @GetMapping(ApiPath.MOVIE + "/{movieId}")
+    // http://localhost:8080/api/movies/{movieId}
+    @GetMapping("/{movieId}")
     public ResponseEntity<MovieDetailResponse> getMovieDetail(
             @PathVariable Long movieId) {
         MovieDetailResponse response = movieService.getMovieDetail(movieId);
         return ResponseEntity.ok(response);
     }
 
-
     // 영화별 리뷰 목록 조회
-    @GetMapping(ApiPath.MOVIE + "/{movieId}/reviews")
+    // http://localhost:8080/api/movies/{movieId}/reviews
+    @GetMapping("/{movieId}/reviews")
     public ResponseEntity<Slice<ReviewResponse>> getMovieReviews(
             @PathVariable Long movieId,
             @RequestParam(required = false) Long cursorId,
