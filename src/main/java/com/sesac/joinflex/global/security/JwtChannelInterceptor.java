fff9034f0ec62303327e78f42c275fa175fe69e1
yu-rim98 <rimmm098@gmail.com>
2026-02-08 14:37:14 +0900

    Feat: 연결 종료 시 퇴장 처리 추가

--- src/main/java/com/sesac/joinflex/global/security/JwtChannelInterceptor.java
+++ src/main/java/com/sesac/joinflex/global/security/JwtChannelInterceptor.java
@@ -1,26 +1,41 @@
 package com.sesac.joinflex.global.security;
 
+import com.sesac.joinflex.domain.chat.dto.MessageType;
+import com.sesac.joinflex.domain.chat.dto.response.ChatMessageResponse;
+import com.sesac.joinflex.domain.party.service.PartyService;
 import com.sesac.joinflex.domain.user.dto.response.UserResponse;
 import com.sesac.joinflex.global.exception.CustomException;
 import com.sesac.joinflex.global.exception.ErrorCode;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class JwtChannelInterceptor implements ChannelInterceptor {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
+    private static final String PARTY_ID_STR = "partyId";
+    private static final String SUBSCRIBE_PARTY_PREFIX = "/sub/party/";
 
     private final JwtProvider jwtProvider;
+    private final PartyService partyService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public JwtChannelInterceptor(JwtProvider jwtProvider, PartyService partyService,
+        @Lazy SimpMessagingTemplate messagingTemplate) {
+        this.jwtProvider = jwtProvider;
+        this.partyService = partyService;
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -29,6 +44,10 @@ public class JwtChannelInterceptor implements ChannelInterceptor {
 
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             handleConnect(accessor);
+        }
+
+        if (StompCommand.DISCONNECT.equals(accessor.getCommand())) {
+            handleDisconnect(accessor);
         }
 
         return message;
@@ -47,6 +66,34 @@ public class JwtChannelInterceptor implements ChannelInterceptor {
         accessor.setUser(authenticationToken);
     }
 
+    private void handleDisconnect(StompHeaderAccessor accessor) {
+
+        UserResponse user = getUser(accessor);
+
+        Long partyId = (Long) accessor.getSessionAttributes().get(PARTY_ID_STR);
+
+        if (user != null && partyId != null) {
+            processLeave(partyId, user);
+        }
+    }
+
+    private void processLeave(Long partyId, UserResponse user) {
+        partyService.leavePartyRoom(partyId, user.getId())
+            .ifPresent(currentCount -> messagingTemplate.convertAndSend(
+                SUBSCRIBE_PARTY_PREFIX + partyId,
+                new ChatMessageResponse(MessageType.LEAVE, user.getNickName(), user.getNickName() + "님이 퇴장하셨습니다.")
+            ));
+    }
+
+    private static UserResponse getUser(StompHeaderAccessor accessor) {
+        Authentication auth = (Authentication) accessor.getUser();
+
+        if (auth != null && auth.getPrincipal() instanceof UserResponse user) {
+            return user;
+        }
+
+        return null;
+    }
 
     private String resolveToken(StompHeaderAccessor accessor) {
         String rawToken = accessor.getFirstNativeHeader(AUTHORIZATION_HEADER);
